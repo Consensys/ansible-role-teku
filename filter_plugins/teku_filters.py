19fe48c34bb7fba2cc9438764c3b9c72a270843e
Edward Evans <edward.joshua.evans@gmail.com>
2020-05-18 16:10:05 +1000

    Add custom filter for setting `TEKU_OPTS`
    Signed-off-by: Edward Evans <edward.joshua.evans@gmail.com>

---
+++ filter_plugins/teku_filters.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from typing import Union, List
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'teku_opts_format': self.tekuOptsFormat,
+        }
+
+    def tekuOptsFormat(self, teku_env_opts: Union[str, List[str]]):
+        if isinstance(teku_env_opts, str):
+            teku_env_opts = teku_env_opts.split()
+
+        escaped_teku_env_opts = [item.replace('"', '\\"') for item in teku_env_opts]
+
+        return f'''"{'" "'.join(escaped_teku_env_opts)}"'''
